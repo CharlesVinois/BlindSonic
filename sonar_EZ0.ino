d8b2999858dceb082649d297fd5355655c5788a4
charles-vinois <charlesvinois@gmail.com>
2017-10-30 02:42:44 +0100

    Add arduino code and library EZO

---
+++ sonar_EZ0.ino
@@ -0,0 +1,26 @@
+#include "SonarEZ0pw.h"
+SonarEZ0pw Sonar(7); // pin D7
+int ledPin = 12;
+float value=0.00;
+float vibration=0.00;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  value = Sonar.Distance(cm);
+  if(value <= 100) // 1 metre max
+  {
+    digitalWrite(ledPin, HIGH);
+    delay(300);
+    digitalWrite(ledPin, LOW);
+    delay(value*7); // coeficient amplificateur
+    Serial.print("Distance= " );
+    Serial.print(value);
+    Serial.println(" cm ");
+   }
+   delay(100);
+}
